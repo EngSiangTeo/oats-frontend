03fedce33d269df39933679007b0c2148d7d1e91
Eng Siang <teo-eng-siang@hotmail.com>
2021-07-09 16:52:43 +0800

    Login Feature

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Chat from './Chat/Chat';
+import Login from './Common/Login';
+
+function App() {
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+  
+  return (
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/chat">
+            <Chat />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
